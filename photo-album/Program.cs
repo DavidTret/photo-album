25dd76723ee67affc803c86eade93b7826a4573e
DavidTret <davidtrettin@gmail.com>
2022-09-09 14:28:44 -0500

    Modified Find() method to pass new unit tests.

--- photo-album/Program.cs
+++ photo-album/Program.cs
@@ -13,10 +13,12 @@ namespace photo_album
     {
         public string Find(string input)
         {
+            // Return if the input is not an int.
             if (!int.TryParse(input, out int value))
-            {
                 return "albumID must be an int!";
-            }
+            // Return if the input is not between 1 - 100.
+            if (value > 100 || value < 1)
+                return "albumID must be between 1 and 100!";
             throw new NotImplementedException();
         }
     }
